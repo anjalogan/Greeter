81a636e06cd8a44d0bd5ba03d0b8b609802fe023
Unknown <anjaelogan@gmail.com>
2018-02-16 11:57:15 -0600

    Display State Works

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		GreeterClass bob = new GreeterClass();
+		System.out.println(bob.toString());
 	}
 
 }
