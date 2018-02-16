5a0577d712c964bcc9212d44745a41b05f97f8df
Unknown <anjaelogan@gmail.com>
2018-02-16 12:43:57 -0600

    Greeter Array Works

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,6 +6,14 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		GreeterClass bob = new GreeterClass();
 		System.out.println(bob.toString());
+		
+		GreeterClass[] array = new GreeterClass[10];
+		for(int x = 0;x<10;x++){
+			array[x] = new GreeterClass();
+		}
+		for(int x = 0;x<10;x++){
+			System.out.println(array[x].toString());
+		}
 	}
 
 }
